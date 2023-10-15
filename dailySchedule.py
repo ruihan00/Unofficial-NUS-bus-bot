6a08c818cb46755bcc397669f0c4e96cb859d519
ruihan00 <e0725640@u.nus.edu>
2023-10-15 23:45:18 +0800

    fix schedule file

--- dailySchedule.py
+++ dailySchedule.py
@@ -3,7 +3,7 @@ import os
 
 
 try:
-    requests.get(url='https://bus-bot.onrender.com/dailyChS4JPkbBt' + "ChS4JPkbBt")
+    requests.get(url='https://bus-bot.onrender.com/dailyChS4JPkbBt')
 
 except Exception as e:
     print(e)
